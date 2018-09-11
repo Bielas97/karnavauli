0894b62fa72fcba69a2a38288ac6efee6afa1948
Kuba <jakub.bielawski@wawasoft.com>
2018-09-11 22:58:55 +0200

    ref

--- src/main/resources/static/js/indexScript.js
+++ src/main/resources/static/js/indexScript.js
@@ -1,7 +1,7 @@
-const BASE_URL = 'http://localhost:8080/';
+/*const BASE_URL = 'http://localhost:8080/';
 let clickMeBtn = document.getElementById('clickMe');
 
-clickMeBtn.addEventListener('click', () => window.location.replace(BASE_URL + 'addCustomer'));
+clickMeBtn.addEventListener('click', () => window.location.replace(BASE_URL + 'addCustomer'));*/
 
 /*clickMeBtn.addEventListener('click', function () {
     window.location.replace(BASE_URL + 'addCustomer')
